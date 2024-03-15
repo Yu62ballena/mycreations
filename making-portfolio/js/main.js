bdd8d9cc0b896b7f5f012685c7c6af73a4427a80
Yu <31719336+Yu62ballena@users.noreply.github.com>
2024-03-15 15:19:13 +0900

    Delete making-portfolio directory

--- making-portfolio/js/main.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.querySelector(".menu-btn");
-const berger = document.querySelector(".hamburger-btn");
-
-menuBtn.addEventListener("click", () => {
-  berger.classList.toggle("active");
-});
